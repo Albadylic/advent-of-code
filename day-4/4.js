96af343d0c705dd9fda7c733b0a9fafd5beede50
Gregor <gregorlthomson@sky.com>
2021-12-05 20:24:57 +0000

    Add functions to check for a number in any grid, check for a bingo in a row, and check for bingo in a column

---
+++ day-4/4.js
@@ -0,0 +1,98 @@
+let inputData = input.split("\n");
+
+const calls = inputData.shift().split(",");
+
+function generateGrids(arr) {
+  let grids = [];
+  let gridNum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "") {
+      gridNum++;
+      grids.push([]);
+    } else {
+      grids[gridNum - 1].push(arr[i].split(" ").filter((item) => item !== ""));
+    }
+  }
+  return grids;
+}
+
+const grids = generateGrids(inputData);
+
+const checkForNum = (arr, num) => {
+  arr.forEach((grid, ind) => {
+    grid.forEach((row) => {
+      if (row.includes(num)) {
+        let index = row.indexOf(num);
+        row[index] = "X";
+      }
+    });
+  });
+};
+
+// Pass in a grid, checks each row to see if there are 5 Xs.
+// Return true or false
+const checkRow = (grid) => {
+  let count = 0;
+
+  for (let j = 0; j < grid.length; j++) {
+    let row = grid[j];
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === "X") {
+        count++;
+        if (count === 5) {
+          return true;
+        }
+      }
+    }
+    count = 0;
+  }
+  return false;
+};
+
+const checkColumn = (grid) => {
+  let count = 0;
+
+  let column = 0;
+  let row = 0;
+
+  while (column < grid.length) {
+    while (row < 5) {
+      if (grid[row][column] === "X") {
+        count++;
+      }
+      console.log({ row }, { column }, { count });
+      row++;
+    }
+    if (count === 5) {
+      return true;
+    }
+    column++;
+    row = 0;
+    count = 0;
+  }
+  return false;
+};
+
+// Currently, checkForNum checks for a specific num
+// Eventually, call this for each num of the calls
+
+// After each checkForNum, check each row and column of each grid
+// The first grid must be found, so specific grids should be iterated
+//  rather than all rows then all columns
+checkForNum(grids, "32");
+checkForNum(grids, "38");
+checkForNum(grids, "56");
+checkForNum(grids, "64");
+checkForNum(grids, "88");
+
+// 25  41  32  30  39
+// 06  66  38  95  05
+// 31  13  56  67  34
+// 69  18  64  44  96
+// 75  14  88  97  40
+
+// 41 66 13 18 14
+console.log(grids[3]);
+//console.log(checkRow(grids[3]));
+console.log(checkColumn(grids[3]));
